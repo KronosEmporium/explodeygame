f0aec638987e31ec2b102e29694c6a1e52140e7b
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 14:46:53 -0700

    Changed some stuff

--- js/game.js
+++ js/game.js
@@ -9,6 +9,7 @@ Game.init = function(){
     Game.canvas = document.createElement('canvas');
     Game.canvas.height = 600;
     Game.canvas.width = 600;
+    document.getElementById('game').appendChild(Game.canvas);
 };
 
 // Game.create = function(){
