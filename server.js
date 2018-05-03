bebe48e4953c7d8d01bc84c11439cc67a1c60164
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 15:12:22 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
-//app.use('/assets',express.static(__dirname + '/assets'));
+app.use('/assets',express.static(__dirname + '/assets'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
@@ -13,7 +13,7 @@ app.get('/',function(req,res){
 
 server.lastPlayderID = 0;
 
-server.listen(process.env.PORT || 8081,function(){
+server.listen(process.env.PORT || 4000,function(){
     console.log('Listening on '+server.address().port);
 });
 
@@ -28,12 +28,12 @@ io.on('connection',function(socket){
         socket.emit('allplayers',getAllPlayers());
         socket.broadcast.emit('newplayer',socket.player);
 
-        socket.on('click',function(data){
-            console.log('click to '+data.x+', '+data.y);
-            socket.player.x = data.x;
-            socket.player.y = data.y;
-            io.emit('move',socket.player);
-        });
+        // socket.on('click',function(data){
+//             console.log('click to '+data.x+', '+data.y);
+//             socket.player.x = data.x;
+//             socket.player.y = data.y;
+//             io.emit('move',socket.player);
+//         });
 
         socket.on('disconnect',function(){
             io.emit('remove',socket.player.id);
