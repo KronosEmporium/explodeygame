348308b0680066b2e1766bb9a5b15792356b65f1
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 19:38:28 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
 var port = process.env.PORT || 8080;
+var server = http.createServer(app).listen(port);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
