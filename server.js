b97ef62bab0b5d5bb1c50017e53f224d9918c4df
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 19:32:04 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use('/css',express.static(__dirname + '/css'));
-app.use('/js',express.static(__dirname + '/js'));
-app.use('/assets',express.static(__dirname + '/assets'));
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
