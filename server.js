f95e9b7857f8fde5f9b579eaba6a3f1c6203974e
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 19:56:46 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http');
 var port = process.env.PORT || 8080;
 var server = http.createServer(app).listen(port);
