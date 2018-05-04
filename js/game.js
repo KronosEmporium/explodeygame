f975c898cc83417151d75487b6373d0f9980d746
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 19:02:41 -0700

    changed some stuff

--- js/game.js
+++ js/game.js
@@ -7,29 +7,25 @@ Game.init = function(){
     document.getElementsByTagName('article')[0].appendChild(Game.canvas);
 };
 
-// Game.create = function(){
-// 
-//     Client.askNewPlayer();
-// };
-// 
-// Game.getCoordinates = function(layer,pointer){
-//     Client.sendClick(pointer.worldX,pointer.worldY);
-// };
-// 
-// Game.addNewPlayer = function(id,x,y){
-//     Game.playerMap[id] = game.add.sprite(x,y,'sprite');
-// };
-// 
-// Game.movePlayer = function(id,x,y){
-//     var player = Game.playerMap[id];
-//     var distance = Phaser.Math.distance(player.x,player.y,x,y);
-//     var tween = game.add.tween(player);
-//     var duration = distance*10;
-//     tween.to({x:x,y:y}, duration);
-//     tween.start();
-// };
-// 
-// Game.removePlayer = function(id){
-//     Game.playerMap[id].destroy();
-//     delete Game.playerMap[id];
-// };
+Game.create = function(){
+
+    Client.askNewPlayer();
+};
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var tween = game.add.tween(player);
+    var duration = distance*10;
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var game = require('./js/game.js');
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	game.init();
 });
 
 http.listen(process.env.PORT || 8081,function(){
