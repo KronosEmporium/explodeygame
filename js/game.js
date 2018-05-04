90c0c8ff68651502b22c229399703a6c741a4596
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 20:38:40 -0700

    Changed some stuff

--- js/game.js
+++ js/game.js
@@ -1,31 +1,11 @@
-var Game = {
-	init : function(){
-    	Game.canvas = document.createElement('canvas');
-    	Game.canvas.height = 600;
-    	Game.canvas.width = 600;
-    	document.body.appendChild(Game.canvas);
-    }
-};
+$(function() {
+	var url = 'https://localhost:8080';
+	var socket = io.connect(url);
+});
 
-// Game.create = function(){
-// 
-//     Client.askNewPlayer();
-// };
-// 
-// Game.addNewPlayer = function(id,x,y){
-//     Game.playerMap[id] = game.add.sprite(x,y,'sprite');
-// };
-// 
-// Game.movePlayer = function(id,x,y){
-//     var player = Game.playerMap[id];
-//     var distance = Phaser.Math.distance(player.x,player.y,x,y);
-//     var tween = game.add.tween(player);
-//     var duration = distance*10;
-//     tween.to({x:x,y:y}, duration);
-//     tween.start();
-// };
-// 
-// Game.removePlayer = function(id){
-//     Game.playerMap[id].destroy();
-//     delete Game.playerMap[id];
+
+// var Game = {
+// 	init : function(){
+//     	
+//     }
 // };
