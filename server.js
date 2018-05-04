c693398d71b305d72dde929d0da02216eed7a674
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 19:36:16 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -10,6 +11,6 @@ io.on('connection', function(socket) {
 	console.log('a user connected');
 });
 
-http.listen(process.env.PORT || 8080,function(){
+http.listen(port,function(){
 	console.log('listening');
 });
