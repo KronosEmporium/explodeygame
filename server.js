915ea4ae8c5bd1ec717dedc665d34fe08f49b6aa
Crosby Allison <crosbyallison@Compyouter.local>
2018-05-03 17:42:47 -0700

    Changed some stuff

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname+'/index.html');
 });
 
 http.listen(3000,function(){
